2804035b079a48f17e1cddfecb04f9902345e234
tiagoc0sta <tiagodpv@gmail.com>
2024-07-28 22:16:13 -0400

    add ListView

---
+++ ToDoList/Views/AddView.swift
@@ -0,0 +1,18 @@
+//
+//  AddView.swift
+//  ToDoList
+//
+//  Created by Tiago de Freitas Costa on 2024-07-28.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddView()
+}
